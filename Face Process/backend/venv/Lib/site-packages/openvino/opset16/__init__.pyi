79b8eac12d2b0a96a6403b51c846f80132f8b1b8
Ngô Chí Kiên <kienlc102@gmail.com>
2025-11-09 01:03:53 +0700

    setup complete

--- Face Process/backend/venv/Lib/site-packages/openvino/opset16/__init__.pyi
+++
@@ -1,8 +0,0 @@
-# type: ignore
-from . import ops
-from __future__ import annotations
-from openvino.opset16.ops import identity
-from openvino.opset16.ops import istft
-from openvino.opset16.ops import segment_max
-from openvino.opset16.ops import sparse_fill_empty_rows
-__all__ = ['identity', 'istft', 'ops', 'segment_max', 'sparse_fill_empty_rows']
